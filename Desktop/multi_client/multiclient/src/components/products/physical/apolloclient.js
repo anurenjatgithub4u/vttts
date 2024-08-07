5722a9e773c94c2f81a8d0ca55f4178b444feb0a
Anurenj Sudheer <124539091+anurenjatgithub4u@users.noreply.github.com>
2024-08-07 16:35:26 +0530

    Delete Desktop/multi_client/multiclient directory

--- Desktop/multi_client/multiclient/src/components/products/physical/apolloclient.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql', // Replace with your GraphQL backend endpoint
-  cache: new InMemoryCache(),
-});
-
-export default client;
